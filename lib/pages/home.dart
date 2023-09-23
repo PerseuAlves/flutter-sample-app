e673ee54f5fbc1dbee4b284e2081d7ab25a7c9a0
PerseuAlves <perseualves3838@outlook.com>
2023-09-23 19:32:18 -0300

    a right button has been added to the 'appBar'

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,7 +21,7 @@ class HomePage extends StatelessWidget {
 
         leading: GestureDetector(
           onTap: () {
-
+            
           }, 
           child: Container(
             margin: const EdgeInsets.all(10),
@@ -37,6 +37,28 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ),
+
+        actions: [
+          GestureDetector(
+            onTap: () {
+
+            },
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              alignment: Alignment.center,
+              width: 37,
+              decoration: BoxDecoration(
+                color: const Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: SvgPicture.asset(
+                'assets/icons/dots.svg',
+                height: 5,
+                width: 5,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
