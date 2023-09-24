08ef437f126304e70c0f87da13ccf2f3baf5d01b
PerseuAlves <perseualves3838@outlook.com>
2023-09-24 00:44:07 -0300

    search bar has been added inside a ‘Column’ widget, a custom decoration has also been set for it

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,33 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0xff1d1617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0,
+                )
+              ]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.all(15),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                )
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
